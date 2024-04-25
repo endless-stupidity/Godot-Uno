5c5b16437725a65e73d6d0fe24b364fcaaf07847
endless-stupidity <mohammad.m6353@gmail.com>
2024-04-25 15:34:08 +0330

    add broken 3d perspective to cards (will fix later)

--- Scripts/Cards/CardFace.gd
+++ Scripts/Cards/CardFace.gd
@@ -1,6 +1,8 @@
-extends Sprite2D
+extends TextureRect
 
 func _ready() -> void:
 	var card_color: String = get_parent().get_meta("Color")
 	var card_value: String = get_parent().get_meta("Value")
 	texture = GameMaster.sprite_map[card_color][card_value]
+
+
--- Scripts/PlayerHand.gd
+++ Scripts/PlayerHand.gd
@@ -3,3 +3,8 @@ extends Area2D
 func clear_player_hand() -> void:
 	for child in get_children():
 		child.queue_free()
+
+func update_player_hand() -> void:
+	clear_player_hand()
+	for card in GameMaster.player_hand:
+		add_child(card)
