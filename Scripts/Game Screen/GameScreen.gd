4798238891d5e9bbc52051fe3d0bdfaa2bfd2fd0
Endless Stupidity <mohammad.m6353@gmail.com>
2024-05-21 16:37:27 +0330

    redo the player's and the 3 cpus' hands to use a single HandManager node instead of 4 seperate scripts

--- Scripts/Game Screen/GameScreen.gd
+++ Scripts/Game Screen/GameScreen.gd
@@ -1,17 +1,16 @@
 extends Node2D
 
-@onready var player_hand = $PlayerHand
+@onready var player_hand = $PlayerHand/HandManager
 @onready var discard_pile = $DiscardPile
-@onready var cpu1_hand = $Cpu1Hand
-@onready var cpu2_hand = $Cpu2Hand
-@onready var cpu3_hand = $Cpu3Hand
+@onready var cpu1_hand = $Cpu1Hand/HandManager
+@onready var cpu2_hand = $Cpu2Hand/HandManager
+@onready var cpu3_hand = $Cpu3Hand/HandManager
 @onready var game_hud = $GameHud
 @onready var color_selector = $ColorSelector
 
 func _ready() -> void:
 	GameMaster.connect("player_hand_changed", _on_GameMaster_player_hand_changed)
 	GameMaster.connect("discard_pile_changed", _on_GameMaster_discard_pile_changed)
-	GameMaster.connect("deck_changed", _on_GameMaster_deck_changed)
 	GameMaster.connect("cpu1_hand_changed", _on_GameMaster_cpu1_hand_changed)
 	GameMaster.connect("cpu2_hand_changed", _on_GameMaster_cpu2_hand_changed)
 	GameMaster.connect("cpu3_hand_changed", _on_GameMaster_cpu3_hand_changed)
@@ -33,22 +32,19 @@ func init_game() -> void:
 	game_hud.change_take_card_button_color(GameMaster.color_map[GameMaster.current_color], 0.5)
 
 func _on_GameMaster_player_hand_changed() -> void:
-	player_hand.update_player_hand()
+	player_hand.update_hand()
+
+func _on_GameMaster_cpu1_hand_changed() -> void:
+	cpu1_hand.update_hand()
+
+func _on_GameMaster_cpu2_hand_changed() -> void:
+	cpu2_hand.update_hand()
+
+func _on_GameMaster_cpu3_hand_changed() -> void:
+	cpu3_hand.update_hand()
 
 func _on_GameMaster_discard_pile_changed() -> void:
 	discard_pile.update_discard_pile()
-
-func _on_GameMaster_deck_changed() -> void:
-	pass
-
-func _on_GameMaster_cpu1_hand_changed() -> void:
-	cpu1_hand.update_cpu1_hand()
-
-func _on_GameMaster_cpu2_hand_changed() -> void:
-	cpu2_hand.update_cpu2_hand()
-
-func _on_GameMaster_cpu3_hand_changed() -> void:
-	cpu3_hand.update_cpu3_hand()
 
 func _on_GameMaster_new_round() -> void:
 	match GameMaster.current_player:
---
+++ Scripts/Game Screen/HandManager.gd
@@ -0,0 +1,126 @@
+extends Node2D
+
+var vertical_card_curve: Curve = preload("res://Assets/Curves/VerticalCardCurve.tres") #the curve for the x offset of the cards
+var horizontal_card_curve: Curve = preload("res://Assets/Curves/HorizontalCardCurve.tres") #the curve for the y offset of the cards
+var card_rotate_curve: Curve = preload("res://Assets/Curves/CardHandRotateCurve.tres") #the curve for the rotation of the cards
+
+@export_enum("Player's Hand", "Cpu1's Hand", "Cpu2's Hand", "Cpu3's Hand") var hand_id: String
+@export var rotation_factor: float = 0.3 #how much should the cards be rotated
+@export var spread_amount: int = 300 #how much should the cards spread on the x axis
+
+func clear_hand() -> void: 
+	for child in get_children():
+			remove_child(child)
+
+func update_hand() -> void: 
+	var tween = create_tween().set_parallel()
+	
+	clear_hand()
+	
+	var card_count: int
+	match hand_id:
+		"Player's Hand":
+			card_count = GameMaster.player_hand.size()
+			if card_count < 6 and card_count > 3: 
+				spread_amount = 100
+			elif card_count < 4:
+				spread_amount = 50
+			elif card_count < 15:
+				spread_amount = 200
+		"Cpu1's Hand":
+			card_count = GameMaster.cpu1_hand.size()
+			if card_count < 6 and card_count > 3: 
+				spread_amount = 50
+			elif card_count < 4:
+				spread_amount = 25
+			elif card_count < 15:
+				spread_amount = 100
+		"Cpu2's Hand":
+			card_count = GameMaster.cpu2_hand.size()
+			if card_count < 6 and card_count > 3: 
+				spread_amount = 50
+			elif card_count < 4:
+				spread_amount = 25
+			elif card_count < 15:
+				spread_amount = 100
+		"Cpu3's Hand":
+			card_count = GameMaster.cpu3_hand.size()
+			if card_count < 6 and card_count > 3: 
+				spread_amount = 50
+			elif card_count < 4:
+				spread_amount = 25
+			elif card_count < 15:
+				spread_amount = 100
+	
+	if card_count > 1:
+		match hand_id:
+			"Player's Hand":
+				for card in GameMaster.player_hand:
+					add_child(card)
+			"Cpu1's Hand":
+				for card in GameMaster.cpu1_hand:
+					add_child(card)
+			"Cpu2's Hand":
+				for card in GameMaster.cpu2_hand:
+					add_child(card)
+			"Cpu3's Hand":
+				for card in GameMaster.cpu3_hand:
+					add_child(card)
+
+		for card in get_children(): #spread, rotate and offset the y axis of the cards based on their index
+			var hand_ratio = float(card.get_index()) / float(card_count - 1)
+			var local_position: Vector2
+			if hand_id == "Player's Hand":
+				var rotation_amount = card_rotate_curve.sample(hand_ratio)
+				card.rotation = rotation_amount * rotation_factor
+				local_position = Vector2(vertical_card_curve.sample(hand_ratio) * spread_amount, horizontal_card_curve.sample(hand_ratio))
+			else:
+				local_position = Vector2(vertical_card_curve.sample(hand_ratio) * spread_amount, 0)
+			tween.tween_property(card, "position", local_position, 0.5)
+			
+			if hand_id == "Player's Hand":
+				card.set_meta("HoverEffect", true)
+				card.set_meta("CanBePlayed", true)
+				card.set_card_back(false)
+			else:
+				card.set_meta("HoverEffect", false)
+				card.set_meta("CanBePlayed", false)
+				card.set_card_back(true)
+			
+	else: #if there's only one card left, make sure to center it
+		var card: Node2D
+		match hand_id:
+			"Player's Hand":
+				card = GameMaster.player_hand[0]
+			"Cpu1's Hand":
+				card = GameMaster.cpu1_hand[0]
+			"Cpu2's Hand":
+				card = GameMaster.cpu2_hand[0]
+			"Cpu3's Hand":
+				card = GameMaster.cpu3_hand[0]
+		 
+		var hand_ratio = 0.5
+		var local_position = Vector2(vertical_card_curve.sample(hand_ratio), vertical_card_curve.sample(hand_ratio))
+		tween.tween_property(card, "position", local_position, 0.5)
+		card.rotation = 0
+		if hand_id == "Player's Hand":
+			card.set_meta("HoverEffect", true)
+			card.set_meta("CanBePlayed", true)
+			card.set_card_back(false)
+		else:
+			card.set_meta("HoverEffect", false)
+			card.set_meta("CanBePlayed", false)
+			card.set_card_back(true)
+		add_child(card)
+
+func can_play(play: bool = true) -> void:
+	if play:
+		for card in get_children():
+			card.get_child(0).material.set_shader_parameter("grayscale", false)
+			card.set_meta("HoverEffect", true)
+			card.set_meta("CanBePlayed", true)
+	else:
+		for card in get_children():
+			card.get_child(0).material.set_shader_parameter("grayscale", true)
+			card.set_meta("HoverEffect", false)
+			card.set_meta("CanBePlayed", false)
