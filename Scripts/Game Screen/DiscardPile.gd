adc6d1ed33e6425d1c18aabc2ed86cd686df73ad
Endless Stupidity <mohammad.m6353@gmail.com>
2024-05-23 18:35:02 +0330

    add a small random rotation to discarded cards

--- Scripts/Game Screen/DiscardPile.gd
+++ Scripts/Game Screen/DiscardPile.gd
@@ -5,9 +5,10 @@ func clear_discard_pile() -> void:
 		remove_child(child)
 
 func update_discard_pile() -> void:
-	var shown_card = GameMaster.discard_pile[-1]
+	var shown_card: Node2D = GameMaster.discard_pile[-1]
 	shown_card.set_meta("HoverEffect", false)
 	shown_card.set_meta("CanBePlayed", false) #make sure a played card can't be played again
 	shown_card.set_card_back(false)
 	shown_card.position = Vector2(0, 0)
+	shown_card.rotate(randf_range(-0.2, 0.2)) 
 	add_child(shown_card)
